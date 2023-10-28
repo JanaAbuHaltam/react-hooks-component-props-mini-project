5ce8ae23ca2c6326722f515de1ca74502246cb1a
Jana Salah <janaabuhaltam@gmail.com>
2023-10-28 15:36:05 +0300

    Completed assignment

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react" 
+
+
+function Header ({name}){
+    return(
+        <div>
+            <header>
+                <h1>{name}</h1>
+                </header>
+            
+
+        </div>
+    )
+}
+
+
+export default Header
